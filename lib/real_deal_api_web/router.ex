5e3f9b14dc186180d38a56178bd1a19d2ea923d7
Zeeshan Haider <zeeshan.haider@techverx.com>
2024-09-12 22:53:33 +0500

    pipeline created

--- lib/real_deal_api_web/router.ex
+++ lib/real_deal_api_web/router.ex
@@ -14,10 +14,19 @@ defmodule RealDealApiWeb.Router do
     plug :accepts, ["json"]
   end
 
+  pipeline :auth do
+    plug RealDealApiWeb.Auth.Pipeline
+  end
+
   scope "/api", RealDealApiWeb do
     pipe_through :api
     get "/", DefaultController, :index
     post "/accounts/create", AccountController, :create
     post "/accounts/sign_in", AccountController, :signin
+  end
+
+  scope "/api", RealDealApiWeb do
+    pipe_through [:app, :auth]
+    get "/accounts/get_by_id/:id", AccountController, :show
   end
 end
